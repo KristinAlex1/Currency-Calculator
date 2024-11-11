332f3566ee0fe034eb3afcf2f5bd5cdcb984dff7
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-11 18:39:55 -0500

    fixed some issues

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const rateBtn = document.querySelector("button");
 
 
 rateBtn.addEventListener("click", () => {
+    console.log("button clicked");
     fetchApi("https://api.currencyapi.com/v3/latest?apikey=cur_live_NYHvSxa3EykLYZb9kLnvuvLWmjcEXo8JYLmPoWF1");
-})
+});
 
 async function fetchApi(url){
     try{
+        console.log("getting data.....");
         const api = await fetch(url);
         const refinedApi = await api.json();
         console.log(refinedApi);
