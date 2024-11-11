a4be847f1e8ac32c4fe7aad63793990b8cca4294
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-11 18:15:32 -0500

    adding logic to the javascript page

---
+++ app.js
@@ -0,0 +1,17 @@
+const rateBtn = document.querySelector("button");
+
+
+rateBtn.addEventListener("click", () => {
+    fetchApi("https://api.currencyapi.com/v3/latest?apikey=cur_live_NYHvSxa3EykLYZb9kLnvuvLWmjcEXo8JYLmPoWF1");
+})
+
+async function fetchApi(url){
+    try{
+        const api = await fetch(url);
+        const refinedApi = await api.json();
+        console.log(refinedApi);
+    }
+    catch(error){
+        console.log("Could not fetch api, error:",error);
+    }
+}
