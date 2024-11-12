ff4ab6945551e0c5190609e9f958218c68b57102
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-11 23:20:41 -0500

    added message function

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const rateBtn = document.querySelector("button");
+const msg = document.querySelector("#message");
 
 
-rateBtn.addEventListener("click", () => {
+rateBtn.addEventListener("click", (event) => {
     
     event.preventDefault();
-    fetchApi("https://api.currencyapi.com/v3/latest?apikey=cur_live_NYHvSxa3EykLYZb9kLnvuvLWmjcEXo8JYLmPoWF1");
-});
+    fetchApi("https://api.currencyapi.com/v3/latest?apikey=cur_live_NYHvSxa3EykLYZb9kLnvuvLWmjcEXo8JYLmPoWF1")
+    .then((data) => {
+        updateMessage(data);
+    })
+    });
 
 async function fetchApi(url){
     try{
         
         const api = await fetch(url);
         const refinedApi = await api.json();
-        console.log(refinedApi);
+        return refinedApi;
     }
     catch(error){
         console.log("Could not fetch api, error:",error);
     }
-}
+};
+
+function updateMessage(data){
+    const message = data.meta.last_updated_at;
+    msg.innerText = `Last updated at ${message}`;
+
+};
+
