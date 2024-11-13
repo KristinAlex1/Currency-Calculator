e0df0085f1d8c045671075e7c98424cb287379a4
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-12 22:11:49 -0500

    logic works, shows value and also shows flags

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ let apiUrl = "https://api.currencyapi.com/v3/latest?apikey=cur_live_NYHvSxa3EykL
 let convertButton = document.querySelector("button");
 let forSelectionOption = document.querySelector("#for-select");
 let toSelectionOption = document.querySelector("#to-select");
+let forImage = document.querySelector("#for-image");
+let toImage = document.querySelector("#to-image");
+let forValue = document.querySelector("#for-input");
+let toValue = document.querySelector("#to-input");
 let currencyValues = {};
 let option1 = [];
-let option2 = []; 
+
 
 
 async function callFirst(){
     
     await fetchApi(apiUrl);
         addValues(currencyValues.data);
+        
 
 }
 
@@ -34,6 +39,7 @@ async function fetchApi(url){
 /// added event when get exchange rate button is clicked
  convertButton.addEventListener("click", async (event) => {
     event.preventDefault();
+    converter();
     
 
     
@@ -53,9 +59,44 @@ function addValues(obj){
 
 }
 
+function converter(){
+    if(!forValue || !forSelectionOption || !toSelectionOption) {
+        alert("Please fill all the fields");
+    }
+
+    else {
+        let forCode = forSelectionOption.value;
+        let toCode = toSelectionOption.value;
+
+        let forExchangeRate = currencyValues.data[forCode].value;
+        let toExchangeRate = currencyValues.data[toCode].value;
+
+        let val = (parseFloat(forValue.value) / forExchangeRate) * toExchangeRate;
+        toValue.value = val.toFixed(3);
+    };
 
 
+    
 
+}
+
+function changeflags(){
+
+    let valFor = forSelectionOption.value;
+    let valTo = toSelectionOption.value;
+
+    let valFor2 = countryList[valFor];
+    let valTo2 = countryList[valTo];
+
+    forImage.src = `https://flagsapi.com/${valFor2}/flat/64.png`;
+    toImage.src = `https://flagsapi.com/${valTo2}/flat/64.png`;
+
+
+}
+
+
+forSelectionOption.addEventListener("change", changeflags);
+toSelectionOption.addEventListener("change", changeflags);
 
 
 
